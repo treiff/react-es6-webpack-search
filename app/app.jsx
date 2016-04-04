bb62ec64cc633e09c32408b14dd2f058646b8dad
Trevor Reiff <trevorreiff@gmail.com>
2016-04-03 21:54:33 -0400

    Basic structure setup

---
+++ app/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Greeting from './components/greeting.jsx';
+
+ReactDOM.render(
+  <Greeting name="cat" />,
+  document.body
+);
---
+++ app/components/greeting.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Greeting extends React.Component {
+  render() {
+    return <div>Hello, {this.props.name}!</div>;
+  }
+}
+
+export default Greeting;
